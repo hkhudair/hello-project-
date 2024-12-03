626302f07e520253729790afb9a6dd498f299009
hkhudair <eng.hkhudair@gmail.com>
2024-12-03 13:54:22 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+ Route::get(uri:'/about', action: function () {
+   $name = 'Heba';
+   // return view('about', ['name' => $name]);
+    // return view('about')->with('name', $name);
+    return view('about',data: compact('name'));  
+}); 
+
+
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+      '1' => 'Technical',
+      '2' => 'Financial',
+      '3' => 'Sales',
+    ];
+    return view('about',data: compact('name', 'departments'));
+});
