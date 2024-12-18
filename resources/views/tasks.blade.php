7f84e31aebdd79e1a1ee36a199520bfab3428c86
hkhudair <eng.hkhudair@gmail.com>
2024-12-18 13:20:39 +0200

    controller commit

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,8 +19,10 @@
         body {
             font-family: 'Lato';
         }
+
     </style>
 </head>
+
 <body id="app-layout">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
@@ -32,81 +35,103 @@
     <div class="container mt-4">
         <div class="offset-md-2 col-md-8">
             <div class="card">
+                @if (isset($task))
                 <div class="card-header">
-                    New Task
+                    Update Task
                 </div>
                 <div class="card-body">
-                    <!-- New Task Form -->
-                    <form action="create" method="POST">
+                    <!-- Update Task Form -->
+                    <form action="{{ url('update') }}" method="POST">
                         @csrf
+                        <input type="hidden" name="id" value="{{ $task->id }}">
                         <!-- Task Name -->
                         <div class="mb-3">
                             <label for="task-name" class="form-label">Task</label>
-                            <input type="text" name="name" id="task-name" class="form-control" value="">
+                            <input type="text" name="name" id="task-name" class="form-control" value="{{ $task->name }}">
                         </div>
 
-                        <!-- Add Task Button -->
+                        <!-- Update Task Button -->
                         <div>
                             <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-plus me-2"></i>Add Task
+                                <i class="fa fa-plus me-2"></i>Update Task
                             </button>
                         </div>
                     </form>
                 </div>
             </div>
+            @else
 
-            <!-- Current Tasks -->
-            <div class="card mt-4">
-                <div class="card-header">
-                    Current Tasks
-                </div>
-                <div class="card-body">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Task</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Task 1</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Task 2</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Task 3</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+
+
+            <div class="card-header">
+                New Task
+            </div>
+            <div class="card-body">
+                <!-- New Task Form -->
+                <form action="create" method="POST">
+                    @csrf
+                    <!-- Task Name -->
+                    <div class="mb-3">
+                        <label for="task-name" class="form-label">Task</label>
+                        <input type="text" name="name" id="task-name" class="form-control" value="">
+                    </div>
+
+                    <!-- Add Task Button -->
+                    <div>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-plus me-2"></i>Add Task
+                        </button>
+                    </div>
+                </form>
+
+            </div>
+
+
+        </div>
+        @endif
+        <!-- Current Tasks -->
+        <div class="card mt-4">
+            <div class="card-header">
+                Current Tasks
+            </div>
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tasks as $task)
+                        <tr>
+                            <td>{{ $task->name }}</td>
+                            <td>
+                                <form action="/delete/{{ $task->id }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fa fa-trash me-2"></i>Delete
+                                    </button>
+                                </form>
+                                <form action="/edit/{{ $task->id }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-trash me-2"></i>Edit
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
             </div>
         </div>
+    </div>
     </div>
 
     <!-- JavaScripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Container\Attributes\DB;
 use Illuminate\Support\Facades\DB as FacadesDB;
 use Illuminate\Support\Facades\Route;
@@ -8,30 +9,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
- Route::get(uri:'/about', action: function () {
-   $name = 'Heba';
-   // return view('about', ['name' => $name]);
+Route::get('/about', function () {
+    $name = "Montaha";
+    $departments = [
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
     // return view('about')->with('name', $name);
-    return view('about',data: compact('name'));  
-}); 
-
+    // return view('about', ['name' => $name]);
+    return view('about', compact('name', 'departments'));
+});
 
 Route::post('/about', function () {
     $name = $_POST['name'];
     $departments = [
-      '1' => 'Technical',
-      '2' => 'Financial',
-      '3' => 'Sales',
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales',
     ];
-    return view('about',data: compact('name', 'departments'));
+    return view('about', compact('name', 'departments'));
 });
 
 Route::get('tasks', function () {
-  return view('tasks');
+    return view('tasks');
 });
 
-Route::post('create', function () {
-  $task_name = $_POST['name'];
-  FacadesDB::table('task')->insert(['name' => $task_name]);
-  return view('tasks');
-});
+Route::get('tasks', [TaskController::class, 'index']);
+Route::post('create', [TaskController::class, 'create']);
+
+Route::post('delete/{id}', [TaskController::class, 'destroy']);
+
+Route::post('edit/{id}', [TaskController::class, 'edit']);
+
+Route::post('update', [TaskController::class, 'update']);
